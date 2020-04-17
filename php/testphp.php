6742768eaacab4c54f237bbe93ea8266a041d5ab
lv7kraus <lv7kraus@gmail.com>
2020-04-17 11:27:49 +0200

    Ahoj

---
+++ php/testphp.php
@@ -0,0 +1,127 @@
+<?php
+
+echo 666;
+echo"<br>";
+print (66.6);
+echo"<br>";
+echo "pórek";
+echo"<br>";
+print(TRUE);
+ echo"koprovka"." segedín";
+ echo"<img src\"GRW_ghosts_1920x1080.jpg\">";
+ echo"\\";
+ 
+ $a = 8;
+echo"<br>";
+$b = 4;
+
+echo "<br>";
+
+var_dump($a+$b);
+
+echo "<br>";
+
+$a = 8;
+echo"<br>";
+$b = 4;
+
+var_dump($a-$b);
+
+echo "<br>";
+
+$a = 8;
+echo"<br>";
+$b = 4;
+
+var_dump($a*$b);
+
+echo "<br>";
+
+$a = 8;
+echo"<br>";
+$b = 4;
+
+var_dump($a/$b);
+
+echo "<br>";
+
+$a = 8;
+echo"<br>";
+$b = 4;
+
+var_dump($a+=$b);
+
+echo "<br>";
+
+$a = 8;
+echo"<br>";
+$b = 4;
+
+var_dump($a-=$b);
+
+echo "<br>";
+
+$a = 8;
+echo"<br>";
+$b = 4;
+
+var_dump($a*=$b);
+
+echo "<br>";
+
+$a = 8;
+echo"<br>";
+$b = 4;
+
+var_dump($a/=$b);
+
+echo "<br>";
+
+$a = 8;
+echo"<br>";
+$b = 4;
+
+var_dump($a==$b);
+
+echo "<br>";
+
+$a = 8;
+echo"<br>";
+$b = 4;
+
+var_dump($a!=$b);
+
+echo "<br>";
+
+$a = 8;
+echo"<br>";
+$b = 4;
+
+var_dump($a>$b);
+
+echo "<br>";
+
+$a = 8;
+echo"<br>";
+$b = 4;
+
+var_dump($a<$b);
+
+echo "<br>";
+
+$a = 8;
+echo"<br>";
+$b = 4;
+
+var_dump($a||$b);
+
+echo "<br>";
+
+$a = 8;
+echo"<br>";
+$b = 4;
+
+var_dump($a&b);
+
+?>
+
